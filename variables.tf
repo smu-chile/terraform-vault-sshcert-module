704a6c223d9bd915d0252ee5eb7fcdfb90864d80
Eduardo Mandry Mila de la Roca <emandry@smu.cl>
2021-12-21 09:05:58 -0300

    Removed presence from variables.tf file

--- variables.tf
+++ variables.tf
@@ -2,31 +2,26 @@ variable "vault_token" {
   type        = string
   description = "Vault token"
   sensitive   = true
-  presence    = required
 }
 
 variable "vault_address" {
   type        = string
   description = "Vault token"
-  presence    = required
 }
 
 variable "path" {
   type        = string
   description = "Path for secrets"
-  presence    = required
 }
 
 variable "role_name" {
   type        = string
   description = "Name for the role in Vault"
-  presence    = required
 }
 
 variable "environment" {
   type        = string
   description = "Environment where the cert is going to be used"
-  presence    = required
 }
 
 
