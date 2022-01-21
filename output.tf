bda59f43755f7a73b619123d47640e55ddf02f32
Eduardo Mandry Mila de la Roca <emandry@smu.cl>
2022-01-21 13:03:26 -0300

    Added chomp to output

--- output.tf
+++ output.tf
@@ -4,5 +4,5 @@ output "environment" {
 }
 
 output "public_key_info" {
-  value = vault_ssh_secret_backend_ca.basic_engine.public_key
+  value = chomp(vault_ssh_secret_backend_ca.basic_engine.public_key)
 }
